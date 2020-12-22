bd062229fb249db64ed4086c9ade01540250e774
Sreelakshmi <72209376+Sreeelakshmi@users.noreply.github.com>
2020-12-22 19:03:57 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,23 @@
+class Plinko{
+    constructor(x,y){
+        var options={
+            isStatic:true,
+            'restitution':1,'friction':0
+        }
+        this.r=10
+        this.body=Bodies.circle(x,y,this.r,options);
+        
+        World.add(world,this.body);
+    
+    }
+    display(){
+        var pos=this.body.position
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(CENTER);
+        fill("white")
+    ellipse(0,0,this.r,this.r);
+    pop();
+    }
+    }
+    
